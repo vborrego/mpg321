d04b77647f226dacedff1168a8f0d723c7b48348
Paul Eipper <paul@e3c.com.br>
2010-04-15 18:53:14 -0300

    Added audio peak info on Remote Mode
    Outputs @I Peak:%d where %d is an int that goes from 0 to 10
     and represents the peak sound level sampled every ~200ms of playback.

--- mad.c
+++ mad.c
@@ -1,15 +1,15 @@
 /*
     mpg321 - a fully free clone of mpg123.
     Copyright (C) 2001 Joe Drew
-    
+
     Originally based heavily upon:
     plaympeg - Sample MPEG player using the SMPEG library
     Copyright (C) 1999 Loki Entertainment Software
-    
+
     Also uses some code from
     mad - MPEG audio decoder
     Copyright (C) 2000-2001 Robert Leslie
-    
+
     Original playlist code contributed by Tobias Bengtsson <tobbe@tobbe.nu>
 
     This program is free software; you can redistribute it and/or modify
@@ -40,6 +40,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <math.h>
 
 unsigned long current_frame=0;
 unsigned long int icy_buf_read;    /* bytes read while looking for tag */
@@ -54,7 +55,7 @@ enum mad_flow read_from_mmap(void *data, struct mad_stream *stream)
 {
     buffer *playbuf = (buffer *)data;
     void *mpegdata = NULL;
-    
+
     /* libmad asks us for more data when it runs out. We don't have any more,
        so we want to quit here. */
     if (status != MPG321_REWINDING && playbuf->done)
@@ -69,7 +70,7 @@ enum mad_flow read_from_mmap(void *data, struct mad_stream *stream)
         fprintf(stderr, "read_from_mmap called when not expected!\n");
         exit(1);
     }
-    
+
     mpegdata = playbuf->buf;
 
     if(status == MPG321_REWINDING)
@@ -85,7 +86,7 @@ enum mad_flow read_from_mmap(void *data, struct mad_stream *stream)
     playbuf->done = 1;
 
     mad_stream_buffer(stream, mpegdata, playbuf->length - (mpegdata - playbuf->buf));
-    
+
     return MAD_FLOW_CONTINUE;
 }
 
@@ -139,13 +140,13 @@ enum mad_flow read_from_fd(void *data, struct mad_stream *stream)
     buffer *playbuf = data;
     int bytes_to_preserve = stream->bufend - stream->next_frame;
     int readbytes;
-    
+
     static int tag_remain;
     static int tag_consumed;
     int meta_consumed;
     int bytes_after_tag, i;
     char *meta_start, *tag_start, *buf_end;
-    
+
     if(playbuf->done)
     {
         status = MPG321_STOPPED;
@@ -157,12 +158,12 @@ enum mad_flow read_from_fd(void *data, struct mad_stream *stream)
         fprintf(stderr, "read_from_fd called when not expected!\n");
         exit(1);
     }
-    
+
     if(!playbuf->buf)
     {
         fprintf(stderr, "read_from_fd called with null buf!\n");
     }
-    
+
     /* need to preserve the bytes which comprise an incomplete
        frame, that mad has passed back to us */
     if (bytes_to_preserve)
@@ -250,9 +251,9 @@ enum mad_flow read_from_fd(void *data, struct mad_stream *stream)
 	}
 
 	mad_stream_buffer(stream, playbuf->buf, readbytes+bytes_to_preserve);
-    
+
     return MAD_FLOW_CONTINUE;
-}    
+}
 
 char * layerstring(enum mad_layer layer)
 {
@@ -268,7 +269,7 @@ char * layerstring(enum mad_layer layer)
             return "?";
     }
 }
-    
+
 char * modestring(enum mad_mode mode)
 {
     switch(mode)
@@ -307,16 +308,16 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
 {
     char long_currenttime_str[14]; /* this *will* fill if you're using 100000+ minute mp3s */
     char long_remaintime_str[14];
-    
+
     buffer *playbuf = (buffer *)data;
     mad_timer_t time_remaining;
-    
+
     if (stop_playing_file)
     {
         stop_playing_file = 0;
         return MAD_FLOW_STOP;
     }
-    
+
     if(options.opt & MPG321_REMOTE_PLAY)
     {
         enum mad_flow mf;
@@ -355,7 +356,7 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
         mad_timer_string(time_remaining, long_remaintime_str, "%.2u:%.2u.%.2u", MAD_UNITS_MINUTES,
                             MAD_UNITS_CENTISECONDS, 0);
     }
-                        
+
     /* update cached table of frames & times */
     if (current_frame <= playbuf->num_frames) /* we only allocate enough for our estimate. */
     {
@@ -363,19 +364,19 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
             * mad_timer_count(header->duration, MAD_UNITS_MILLISECONDS);
         playbuf->times[current_frame] = current_time;
     }
-    
+
     if (file_change)
     {
         file_change = 0;
         if (options.opt & MPG321_REMOTE_PLAY)
         {
             printf("@S 1.0 %d %d %s %d %ld %d %d %d %d %ld %d\n", header->layer, header->samplerate,
-                modestringucase(header->mode), header->mode_extension, 
+                modestringucase(header->mode), header->mode_extension,
                 (header->bitrate / 8 / 100) * mad_timer_count(header->duration, MAD_UNITS_CENTISECONDS),
-                MAD_NCHANNELS(header), header->flags & MAD_FLAG_COPYRIGHT ? 1 : 0, 
+                MAD_NCHANNELS(header), header->flags & MAD_FLAG_COPYRIGHT ? 1 : 0,
                 header->flags & MAD_FLAG_PROTECTION ? 1 : 0, header->emphasis,
                 header->bitrate/1000, header->mode_extension);
-        }    
+        }
 
         else if (options.opt & MPG321_VERBOSE_PLAY)/*zip it good*/
         {
@@ -383,7 +384,7 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
                     "Channels: %d, copyright: %s, original: %s, CRC: %s, emphasis: %d.\n"
                     "Bitrate: %ld Kbits/s, Extension value: %d\n"
                     "Audio: 1:1 conversion, rate: %d, encoding: signed 16 bit, channels: %d\n",
-                    layerstring(header->layer), header->samplerate, modestringucase(header->mode), header->mode_extension, 
+                    layerstring(header->layer), header->samplerate, modestringucase(header->mode), header->mode_extension,
                     (header->bitrate / 100) * mad_timer_count(header->duration, MAD_UNITS_CENTISECONDS),
                     MAD_NCHANNELS(header), header->flags & MAD_FLAG_COPYRIGHT ? "Yes" : "No",
                     header->flags & MAD_FLAG_ORIGINAL ? "Yes" : "No", header->flags & MAD_FLAG_PROTECTION ? "Yes" : "No",
@@ -397,7 +398,7 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
                 layerstring(header->layer), header->bitrate/1000, header->samplerate, modestring(header->mode));
         }
     }
-    
+
     if (status == MPG321_SEEKING && options.seek)
     {
         if (!--options.seek)
@@ -412,23 +413,23 @@ enum mad_flow read_header(void *data, struct mad_header const * header)
 
     if (options.opt & MPG321_VERBOSE_PLAY)
     {
-        if (!options.skip_printing_frames 
+        if (!options.skip_printing_frames
             || (options.skip_printing_frames && !(current_frame % options.skip_printing_frames)))
-            fprintf(stderr, "Frame# %5lu [%5lu], Time: %s [%s], \r", current_frame, 
+            fprintf(stderr, "Frame# %5lu [%5lu], Time: %s [%s], \r", current_frame,
                     playbuf->num_frames > 0 ? playbuf->num_frames - current_frame : 0, long_currenttime_str, long_remaintime_str);
     }
-    
+
     else if (options.opt & MPG321_REMOTE_PLAY)
     {
-        if (!options.skip_printing_frames 
+        if (!options.skip_printing_frames
             || (options.skip_printing_frames && !(current_frame % options.skip_printing_frames)))
             printf("@F %ld %ld %.2f %.2f\n", current_frame, playbuf->num_frames - current_frame,
                 ((double)mad_timer_count(current_time, MAD_UNITS_CENTISECONDS)/100.0),
                 ((double)mad_timer_count(time_remaining, MAD_UNITS_CENTISECONDS)/100.0));
     }
-    
+
     return MAD_FLOW_CONTINUE;
-}        
+}
 
 /* XING parsing is from the MAD winamp input plugin */
 
@@ -502,14 +503,14 @@ int parse_xing(struct xing *xing, struct mad_bitptr ptr, unsigned int bitlen)
 }
 
 
-/* Following two functions are adapted from mad_timer, from the 
+/* Following two functions are adapted from mad_timer, from the
    libmad distribution */
 void scan(void const *ptr, ssize_t len, buffer *buf)
 {
     struct mad_stream stream;
     struct mad_header header;
     struct xing xing;
-    
+
     unsigned long bitrate = 0;
     int has_xing = 0;
     int is_vbr = 0;
@@ -525,7 +526,7 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
       1) Constant bitrate: One frame can provide the information
          needed: # of frames and duration. Just see how long it
          is and do the division.
-      2) Variable bitrate: Xing tag. It provides the number of 
+      2) Variable bitrate: Xing tag. It provides the number of
          frames. Each frame has the same number of samples, so
          just use that.
       3) All: Count up the frames and duration of each frames
@@ -549,7 +550,7 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
             if(parse_xing(&xing, stream.anc_ptr, stream.anc_bitlen))
             {
                 is_vbr = 1;
-                
+
                 if (xing.flags & XING_FRAMES)
                 {
                     /* We use the Xing tag only for frames. If it doesn't have that
@@ -560,7 +561,7 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
                     break;
                 }
             }
-        }                
+        }
 
         /* Test the first n frames to see if this is a VBR file */
         if (!is_vbr && !(buf->num_frames > 20))
@@ -569,20 +570,20 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
             {
                 is_vbr = 1;
             }
-            
+
             else
             {
                 bitrate = header.bitrate;
             }
         }
-        
+
         /* We have to assume it's not a VBR file if it hasn't already been
            marked as one and we've checked n frames for different bitrates */
         else if (!is_vbr)
         {
             break;
         }
-            
+
        /* In quiet mode, don't scan the whole file for length since
            it takes so long on slow file systems like sshfs. cplay
            calls mpg321 for playback in quiet mode. */
@@ -600,14 +601,14 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
         double time = (len * 8.0) / (header.bitrate); /* time in seconds */
         double timefrac = (double)time - ((long)(time));
         long nsamples = 32 * MAD_NSBSAMPLES(&header); /* samples per frame */
-        
+
         /* samplerate is a constant */
         buf->num_frames = (long) (time * header.samplerate / nsamples);
 
         mad_timer_set(&buf->duration, (long)time, (long)(timefrac*100), 100);
        }
     }
-        
+
     else if (has_xing)
     {
         /* modify header.duration since we don't need it anymore */
@@ -620,7 +621,7 @@ void scan(void const *ptr, ssize_t len, buffer *buf)
         /* the durations have been added up, and the number of frames
            counted. We do nothing here. */
     }
-    
+
     mad_header_finish(&header);
     mad_stream_finish(&stream);
 }
@@ -629,14 +630,14 @@ void pause_play(buffer *buf, playlist *pl)
 {
     static char file[PATH_MAX] = "";
     static signed long seek = 0;
-    
+
     if (buf == NULL && pl == NULL) /* reset */
     {
         strcpy(file, "");
         seek = 0;
         return;
     }
-    
+
     /* pause */
     if (strlen(file) == 0)
     {
@@ -647,7 +648,7 @@ void pause_play(buffer *buf, playlist *pl)
         seek = current_frame;
         printf("@P 1\n");
     }
-    
+
     /* unpause */
     else
     {
@@ -675,7 +676,7 @@ enum mad_flow move(buffer *buf, signed long frames)
 {
     if (frames == 0)
         return 0;
-    
+
     /* Our normal skipping (for -k) code handles forward seeks.
        Rewinds are handled by a stop in decoding, a rewind, and
        a restart in decoding, implemented in the main loop and in the other
@@ -701,7 +702,7 @@ enum mad_flow move(buffer *buf, signed long frames)
 
     return 0;
 }
-    
+
 int calc_length(char *file, buffer *buf)
 {
     int f;
@@ -739,19 +740,19 @@ int calc_length(char *file, buffer *buf)
         /* File must be very short or empty. Forget it. */
         close(f);
         return -1;
-    }    
+    }
 
     if (read(f, buffer, 3) != 3)
     {
         close(f);
         return -1;
     }
-    
+
     if (!strncmp(buffer, "TAG", 3))
     {
         buf->length -= 128; /* Correct for id3 tags */
     }
-    
+
     fdm = mmap(0, buf->length, PROT_READ, MAP_SHARED, f, 0);
     if (fdm == MAP_FAILED)
     {
@@ -863,7 +864,7 @@ enum mad_flow output(void *data,
 {
     register int nsamples = pcm->length;
     mad_fixed_t const *left_ch = pcm->samples[0], *right_ch = pcm->samples[1];
-    
+
     static unsigned char stream[1152*4]; /* 1152 because that's what mad has as a max; *4 because
                                     there are 4 distinct bytes per sample (in 2 channel case) */
     static unsigned int rate = 0;
@@ -880,7 +881,7 @@ enum mad_flow output(void *data,
     {
         channels = MAD_NCHANNELS(header);
         rate = header->samplerate;
-        open_ao_playdevice(header);        
+        open_ao_playdevice(header);
     }
 
     else if ((channels != MAD_NCHANNELS(header) || rate != header->samplerate) && playdevice_is_live())
@@ -888,8 +889,11 @@ enum mad_flow output(void *data,
         ao_close(playdevice);
         channels = MAD_NCHANNELS(header);
         rate = header->samplerate;
-        open_ao_playdevice(header);        
-    }        
+        open_ao_playdevice(header);
+    }
+
+    static int peak_rate = 0;
+    static unsigned short int peak = 0;
 
     if (pcm->channels == 2)
     {
@@ -898,6 +902,8 @@ enum mad_flow output(void *data,
             tempsample = (mad_fixed_t)((*left_ch++ * (double)options.volume)/MAD_F_ONE);
             sample = (signed int) audio_linear_dither(16, tempsample, &dither);
 
+            peak = abs(sample) > peak ? abs(sample) : peak;
+
 #ifndef WORDS_BIGENDIAN
             *ptr++ = (unsigned char) (sample >> 0);
             *ptr++ = (unsigned char) (sample >> 8);
@@ -905,9 +911,12 @@ enum mad_flow output(void *data,
             *ptr++ = (unsigned char) (sample >> 8);
             *ptr++ = (unsigned char) (sample >> 0);
 #endif
-            
+
             tempsample = (mad_fixed_t)((*right_ch++ * (double)options.volume)/MAD_F_ONE);
             sample = (signed int) audio_linear_dither(16, tempsample, &dither);
+
+            peak = abs(sample) > peak ? abs(sample) : peak;
+
 #ifndef WORDS_BIGENDIAN
             *ptr++ = (unsigned char) (sample >> 0);
             *ptr++ = (unsigned char) (sample >> 8);
@@ -919,14 +928,16 @@ enum mad_flow output(void *data,
 
         ao_play(playdevice, stream, pcm->length * 4);
     }
-    
+
     else if (options.opt & MPG321_FORCE_STEREO)
     {
         while (nsamples--)
         {
             tempsample = (mad_fixed_t)((*left_ch++ * (double)options.volume)/MAD_F_ONE);
             sample = (signed int) audio_linear_dither(16, tempsample, &dither);
-            
+
+            peak = abs(sample) > peak ? abs(sample) : peak;
+
             /* Just duplicate the sample across both channels. */
 #ifndef WORDS_BIGENDIAN
             *ptr++ = (unsigned char) (sample >> 0);
@@ -943,14 +954,16 @@ enum mad_flow output(void *data,
 
         ao_play(playdevice, stream, pcm->length * 4);
     }
-        
+
     else /* Just straight mono output */
     {
         while (nsamples--)
         {
             tempsample = (mad_fixed_t)((*left_ch++ * (double)options.volume)/MAD_F_ONE);
             sample = (signed int) audio_linear_dither(16, tempsample, &dither);
-            
+
+            peak = abs(sample) > peak ? abs(sample) : peak;
+
 #ifndef WORDS_BIGENDIAN
             *ptr++ = (unsigned char) (sample >> 0);
             *ptr++ = (unsigned char) (sample >> 8);
@@ -962,5 +975,17 @@ enum mad_flow output(void *data,
         ao_play(playdevice, stream, pcm->length * 2);
     }
 
-    return MAD_FLOW_CONTINUE;        
+    if (options.opt & MPG321_REMOTE_PLAY)
+    {
+        peak_rate = (peak_rate + 1) % 7;
+        if (!peak_rate)
+        {
+            // exponential scale with range of 0 to 10
+            int peak_scale = (int)round(10 * (peak / 32768.0));
+            printf("@I Peak:%d\n", peak_scale);
+            peak = 0;
+        }
+    }
+
+    return MAD_FLOW_CONTINUE;
 }
