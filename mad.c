c605bdba557c8ab832a9951266656f42fe06a37e
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2010-05-13 17:35:55 -0300

    Stop printing Peak if FFT is enabled
    And prefix FFT with the correct value

--- mad.c
+++ mad.c
@@ -864,7 +864,7 @@ static void perform_and_print(char* samples, fft_state *state)
     static int xranges[] = {0, 1, 2, 3, 5, 7, 10, 14, 20, 28, 40, 54, 74, 101, 137, 187, 255};
     static double fftout[FFT_BUFFER_SIZE / 2 + 1];
 
-    printf("@I Peak:");
+    printf("@I FFT:");
     fft_perform((sound_sample*)samples, fftout, state);
 
     int i;
@@ -1047,7 +1047,7 @@ enum mad_flow output(void *data,
         ao_play(playdevice, stream, pcm->length * 2);
     }
 
-    if (options.opt & MPG321_REMOTE_PLAY)
+    if (options.opt & MPG321_REMOTE_PLAY && !(options.opt & MPG321_PRINT_FFT))
     {
         peak_rate = (peak_rate + 1) % 7;
         if (!peak_rate)
