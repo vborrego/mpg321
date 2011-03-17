31bdf8714bd0843dd4f63212334c47ee21f1ead0
Paul Eipper <paul@e3c.com.br>
2011-03-17 17:35:41 -0300

    Configure script with required parameters for SH4

---
+++ mpg321-configure.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ac_cv_func_malloc_0_nonnull=yes ../configure --host=sh4-linux --disable-aotest CFLAGS=-I/opt/STM/STLinux-2.3/devkit/sh4/target/usr/local/include LDFLAGS=-L/opt/STM/STLinux-2.3/devkit/sh4/target/usr/local/lib
